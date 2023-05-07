c200cbca296014a2e4308d8410976802369720a5
Kirill2690 <kirillmuravev2690@gmail.com>
2023-05-07 14:00:12 +0300

    configure:prettierrc

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,25 +1,24 @@
-import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
-import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
-import {combineReducers} from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import {appReducer} from './app-reducer'
-import {authReducer} from '../features/Login/auth-reducer'
-import {configureStore} from "@reduxjs/toolkit";
-
+import { tasksReducer } from "../features/TodolistsList/tasks-reducer";
+import { todolistsReducer } from "../features/TodolistsList/todolists-reducer";
+import { combineReducers } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { appReducer } from "./app-reducer";
+import { authReducer } from "../features/Login/auth-reducer";
+import { configureStore } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    todolists: todolistsReducer,
-    app: appReducer,
-    auth: authReducer
-})
+  tasks: tasksReducer,
+  todolists: todolistsReducer,
+  app: appReducer,
+  auth: authReducer,
+});
 // непосредственно создаём store
 export const store = configureStore({
-    reducer: rootReducer,
-    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)//вначало getDefaultMiddleware добавляем thunkMiddleware
-})
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware), //вначало getDefaultMiddleware добавляем thunkMiddleware
+});
 // определить автоматически тип всего объекта состояния
-export type AppRootStateType = ReturnType<typeof rootReducer>
+export type AppRootStateType = ReturnType<typeof rootReducer>;
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import createRoot from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import {Provider} from "react-redux";
-import {store} from './app/store';
-import {HashRouter} from "react-router-dom";
+import React from "react";
+import createRoot from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { HashRouter } from "react-router-dom";
 import App from "./app/App";
 
 createRoot.render(
-    <HashRouter>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </HashRouter>
-    , document.getElementById('root'));
+  <HashRouter>
+    <Provider store={store}></Provider>
+  </HashRouter>,
+  document.getElementById("root")
+);
 
 // в gh-pages лучше работает HashRouter
 
