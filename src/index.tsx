a65c691267b3b0dd08e98a3f9194faf12fc38919
Kirill2690 <kirillmuravev2690@gmail.com>
2023-05-09 19:18:20 +0300

    add:new file

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,15 @@
-import React from "react";
-import createRoot from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import { HashRouter } from "react-router-dom";
-import App from "./app/App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './app/App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
-createRoot.render(
-  <HashRouter>
-    <Provider store={store}></Provider>
-  </HashRouter>,
-  document.getElementById("root")
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+	<Provider store={store}>
+		<App/>
+	</Provider>
 );
 
-// в gh-pages лучше работает HashRouter
-
-serviceWorker.unregister();
